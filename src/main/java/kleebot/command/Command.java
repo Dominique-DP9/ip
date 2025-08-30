9daba5eb533693c0d32f85ba56ac4a9c5ed5f973
Dominique-DP9 <e1482079@u.nus.edu>
2025-08-30 20:28:19 +0800

    Add auto-configuration for coding standard

--- src/main/java/kleebot/command/Command.java
+++ src/main/java/kleebot/command/Command.java
@@ -7,6 +7,7 @@ import kleebot.ui.Ui;
 
 public abstract class Command {
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws KleeExceptions;
+    
     public boolean isExit() {
         return false;
     }
