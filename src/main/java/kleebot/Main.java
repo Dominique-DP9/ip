54b8db9558d250cf6bb53293651da6fe5190c5a3
Dominique-DP9 <e1482079@u.nus.edu>
2025-09-05 11:23:38 +0800

    Add more GUI features to KleeBot

--- src/main/java/kleebot/Main.java
+++ src/main/java/kleebot/Main.java
@@ -42,7 +42,13 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+
+            stage.setTitle("KleeBot!! <3");
+            Image kleeIcon = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/images/kleeIcon.jpg")));
+            stage.getIcons().add(kleeIcon);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setKlee(kleeBot);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
