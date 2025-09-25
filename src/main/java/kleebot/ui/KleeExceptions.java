1fc101f472974a211445c4c1e87efdc5995476c9
Dominique-DP9 <e1482079@u.nus.edu>
2025-09-26 06:51:38 +0800

    Add javadocs comments

--- src/main/java/kleebot/ui/KleeExceptions.java
+++ src/main/java/kleebot/ui/KleeExceptions.java
@@ -1,5 +1,9 @@
 package kleebot.ui;
 
+
+/**
+ * Represents exceptions used for KleeBot operations
+ */
 public class KleeExceptions extends Exception {
     public KleeExceptions(String m) {
         super(m);
